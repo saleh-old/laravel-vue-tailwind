daed2fea4e45919b8e2f99e9ac26b45bea883342
Sully Fischer <fischersully@gmail.com>
2019-11-30 15:31:06 +0330

    prepare for testing

---
+++ tests/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function create($class, $attributes = [])
+{
+    return factory($class)->create($attributes);
+}
+
+function make($class, $attributes = [])
+{
+    return factory($class)->make($attributes);
+}
+
