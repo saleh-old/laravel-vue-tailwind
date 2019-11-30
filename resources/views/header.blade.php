e2f629a9db8fed6b39028ee800b6505bf1077707
Sully Fischer <fischersully@gmail.com>
2019-11-30 23:12:32 +0330

    implement header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,1 +1,8 @@
-header
+<app-header
+    :brand="'{{ config('app.name', 'Laravel') }}'"
+    :links="[
+        {href: '/about', name: 'About'},
+        {href: '/contact', name: 'Contact'}
+    ]"
+    :auth="{{ Auth::check() ? 'true' : 'false' }}"
+></app-header>
