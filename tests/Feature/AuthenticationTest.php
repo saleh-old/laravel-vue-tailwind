e69d24086e39d963c81f173325f57499a4826acb
Sully Fischer <fischersully@gmail.com>
2019-12-03 22:12:00 +0330

    test login and register functionalities

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,21 +2,55 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_user_can_register()
+    {
+        $response = $this->get('/register');
+        $response->assertSee('Sign Up');
+
+        $response = $this->post('/register', [
+            'name' => 'tester',
+            'email' => 'me@example.com',
+            'password' => 'example-password',
+            'password_confirmation' => 'example-password'
+        ]);
+
+        $response->assertRedirect('/');
+
+        $this->assertAuthenticated();
+    }
+
+    /** @test */
+    public function a_user_can_login()
+    {
+        // register
+        $response = $this->post('/register', [
+            'name' => 'tester',
+            'email' => 'me@example.com',
+            'password' => 'example-password',
+            'password_confirmation' => 'example-password'
+        ]);
+        $response->assertRedirect('/');
+        $this->assertAuthenticated();
+
+        // logout
+        $this->post('/logout');
+
+        // log in
+        $response = $this->post('/login', [
+            'email' => 'me@example.com',
+            'password' => 'example-password',
+        ]);
+        $response->assertRedirect('/');
+        $this->assertAuthenticated();
     }
 }
