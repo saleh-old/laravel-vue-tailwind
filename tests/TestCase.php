10667c6eabae2c28b7d49d665c524e8cc4f80e76
Sully Fischer <fischersully@gmail.com>
2019-12-09 01:30:47 +0330

    fix imports

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -19,13 +21,16 @@ abstract class TestCase extends BaseTestCase
 
     protected function disableExceptionHandling()
     {
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-            public function report(\Exception $e) {}
-            public function render($request, \Exception $e) {
+        $this->app->instance(ExceptionHandler::class, new class extends Handler
+        {
+            public function __construct()
+            { }
+            public function report(\Exception $e)
+            { }
+            public function render($request, \Exception $e)
+            {
                 throw $e;
             }
         });
     }
-
 }
